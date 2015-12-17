7c163d100bce3a16c902192765d5002d8292bb74
Andriy Yatsynyak <andriychea@gmail.com>
2015-12-17 16:47:53 +0200

    add string user in settings

--- microsocial/settings.py
+++ microsocial/settings.py
@@ -87,4 +87,6 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+AUTH_USER_MODEL = 'microsocial.User'
+
 TEMPLATE_DIRS = (os.path.join(BASE_DIR,  'templates'),)
